70a9151264546621c629a8b229609f3fd6d4850b
blckpi <gilcivanlima@gmail.com>
2026-01-03 17:55:50 -0300

    Add base configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "./",
+});
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
